f684b3eb206bb542aa3c4fd01c2132e629ede7d0
babka40000 <it-master@list.ru>
2023-01-17 10:54:59 +0300

    step2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,29 +4,22 @@ taskForm = document.getElementById('tasks__form')
 taskForm.onsubmit = () => {
     taskValue = taskForm.querySelector('.tasks__input').value.trim();
     if (taskValue != '') {
-        taskList.innerHTML += `
+
+        taskList.insertAdjacentHTML('beforeend', `
             <div class="task">
                 <div class="task__title">` +
                     taskValue
                 + `</div>
                 <a href="#" class="task__remove">&times;</a>
-            </div>`;
-        
-        Array.from(taskList.querySelectorAll('.task__remove')).forEach(element => {
-            element.onclick = (elem) => {
-                console.log('1111');
-                taskList.removeChild(elem.srcElement.parentElement);
-                return false;
-            }
-        })
+            </div>`);
+
+        taskList.lastElementChild.querySelector('.task__remove').onclick = (elem) => {
+            taskList.removeChild(elem.srcElement.parentElement);
+            return false;
+        }
+            
+        taskForm.querySelector('.tasks__input').value = '';
     }
 
     return false;
 }
-
-
-
-
-
-
-// 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,14 @@
 Array.from(document.getElementsByClassName('has-tooltip')).forEach(element => {
-    tip = document.createElement('div');
+    let tip = document.createElement('div');
     tip.classList.add('tooltip');
     tip.textContent = element.title;
     element.insertAdjacentElement("beforeend", tip);
 
     element.onclick = () => {
         tip = Array.from(element.getElementsByClassName('tooltip'))[0];
-        tip.classList.add('tooltip_active');
+        tip.classList.toggle('tooltip_active');
         tip.setAttribute('style', 'left: ' + element.offsetLeft + 'px; top: ' + (element.offsetTop - Math.round(window.scrollY)+25) + 'px');
 
         return false;
     }
-
-    element.onmouseout = () => {
-        tip = Array.from(element.getElementsByClassName('tooltip'))[0];
-        if (tip.classList.contains('tooltip_active')) {
-            tip.classList.remove('tooltip_active');
-        }       
-    } 
 })
